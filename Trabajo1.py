ef88ff95769365e236738a711f40f39c41c5a1a3
Legofan123e <111817133+Legofan123e@users.noreply.github.com>
2024-12-06 17:32:20 -0500

    Punto de partida

---
+++ Trabajo1.py
@@ -0,0 +1,12 @@
+import random
+datos= [
+        "Soy estudiante",
+        "Vengo de Ecuador",
+        "Tengo 2 mascotas (ambos perros)",
+        "Me encantan las papas con pollo",
+        "Juego Roblox",
+        "Queria añadir sonidos al proyecto de graduación, pero aparentemente el HUB no tenía soporte para sonidos :(",
+        "Las físicas de gravedad en los Sonic clásicos me fascinan"
+        ]
+selected_data = random.choice(datos)
+print(selected_data)
